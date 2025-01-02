4ee00447093670416b7dbdd02a2bb79797999173
Atul Kumar Sharma <49794348+AtulKrSharma@users.noreply.github.com>
2025-01-02 18:56:36 -0500

    Updated arrayExporter.ts
    --Updated arrayExporter.ts--

--- tests/utils/arrayExporter.ts
+++ tests/utils/arrayExporter.ts
@@ -1,5 +1,13 @@
 export const dummyArray: string[] = [
   'atul',
+  'desiBaba',
+  'baks',
+  'annuj Vyas',
+  'montecarlo',
+];
+
+export const dumberArray: string[] = [
+  'atul',
   'deswal',
   'baks',
   'annu',
