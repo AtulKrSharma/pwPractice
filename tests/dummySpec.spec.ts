f5cb8be427ce1124e5f24cdf3fd24094b194c68f
Atul Kumar Sharma <49794348+AtulKrSharma@users.noreply.github.com>
2025-01-02 15:44:37 -0500

    Update dummySpec.spec.ts- added X into that
    Update dummySpec.spec.ts- added X into that

--- tests/dummySpec.spec.ts
+++ tests/dummySpec.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('dummy scenarios', () => {
-  test('test-1', async ({ page }) => {
+  test('testX-1', async ({ page }) => {
     await page.goto('https://www.timesnownews.com/');
     expect(page.title).toBe('timesNow1');
   });
-  test('test-11', async ({ page }) => {
+  test('testX-11', async ({ page }) => {
     await page.goto('https://www.timesnownews.com/');
     expect(page.title).toBe('timesNow11');
   });
-  test('test-111', async ({ page }) => {
+  test('testX-111', async ({ page }) => {
     await page.goto('https://www.timesnownews.com/');
     expect(page.title).toBe('timesNow111');
   });
