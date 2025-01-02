c1e8e4357555cead4eca3433702e48f7346b3646
Atul Kumar Sharma <sharma.atulkumar29@gmail.com>
2025-01-02 15:37:40 -0500

    PR comments fixed

--- tests/dummySpec.spec.ts
+++ tests/dummySpec.spec.ts
@@ -3,8 +3,6 @@ import { test, expect } from '@playwright/test';
 test.describe('dummy scenarios', () => {
   test('test-1', async ({ page }) => {
     await page.goto('https://www.timesnownews.com/');
-    page.setDefaultTimeout(10000);
     expect(page.title).toBe('timesNow');
-    page.setDefaultTimeout(10000);
   });
 });
