d3d5d2bbd327cd9091ce032afe17b2131b9a127a
Atul Kumar Sharma <49794348+AtulKrSharma@users.noreply.github.com>
2025-01-02 15:43:52 -0500

    Update dummySpec.spec.ts

--- tests/dummySpec.spec.ts
+++ tests/dummySpec.spec.ts
@@ -3,6 +3,14 @@ import { test, expect } from '@playwright/test';
 test.describe('dummy scenarios', () => {
   test('test-1', async ({ page }) => {
     await page.goto('https://www.timesnownews.com/');
-    expect(page.title).toBe('timesNow');
+    expect(page.title).toBe('timesNow1');
+  });
+  test('test-11', async ({ page }) => {
+    await page.goto('https://www.timesnownews.com/');
+    expect(page.title).toBe('timesNow11');
+  });
+  test('test-111', async ({ page }) => {
+    await page.goto('https://www.timesnownews.com/');
+    expect(page.title).toBe('timesNow111');
   });
 });
